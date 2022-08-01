24373f68248c546903c9e3969ff703355e63f551
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-08-01 10:06:20 -0300

    [feat]: requisito 23 implementando rota que deleta uma moto e seu testes - done

--- src/routes/motorcycle.ts
+++ src/routes/motorcycle.ts
@@ -13,6 +13,6 @@ route.post('/', (req, res) => motocycleController.create(req, res));
 route.get('/', (req, res) => motocycleController.read(req, res));
 route.get('/:id', (req, res) => motocycleController.readOne(req, res));
 route.put('/:id', (req, res) => motocycleController.update(req, res));
-route.put('/:id', (req, res) => motocycleController.delete(req, res));
+route.delete('/:id', (req, res) => motocycleController.delete(req, res));
 
 export default route;
